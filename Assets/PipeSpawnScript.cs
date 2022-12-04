bda450125a65ddf812665e423f9e575dbd5919df
kubatokarski <kuba.tokarski@gmail.com>
2022-12-04 13:00:26 +0100

    Pipe Spawner updated.

--- Assets/PipeSpawnScript.cs
+++ Assets/PipeSpawnScript.cs
@@ -5,16 +5,30 @@ using UnityEngine;
 public class PipeSpawnScript : MonoBehaviour
 {
     public GameObject pipe;
+    public float spawnRate = 2;
+    private float timer = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnPipe();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Instantiate(pipe, transform.position, transform.rotation);   
+        if (timer < spawnRate)
+        {
+            timer += Time.deltaTime;
+        } else
+        {
+            spawnPipe();
+            timer = 0;
+        }
+    }
+
+    void spawnPipe()
+    {
+        Instantiate(pipe, transform.position, transform.rotation);
     }
 }
